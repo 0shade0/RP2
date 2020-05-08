5b521e41a85398af8f798f1e8cbc32b6f199eab9
Tihana Stifanic <sttihan@student.math.hr>
2020-05-08 21:04:00 +0200

    Dodan model user

--- model/user.class.php
+++ model/user.class.php
@@ -1,1 +1,41 @@
-<?php?>
+<?php
+class User {
+    protected $ID;
+    protected $ID_household;
+    protected $username;
+    protected $password;
+    protected $email;
+    protected $points;
+    protected $admin;
+    protected $registration_sequence;
+    protected $registered;
+
+    // Konstruktor.
+    public function __construct ($ID, $ID_household, $username, $password,
+        $email, $points, $admin, $registration_sequence, $registered) {
+        $this->ID = $ID;
+        $this->ID_household = $ID_household;
+        $this->username = $username;
+        $this->password = $password;
+        $this->email = $email;
+        $this->points = $points;
+        $this->admin = $admin;
+        $this->registration_sequence = $registration_sequence;
+        $this->registered = $registered;
+    }
+
+    // Getteri i setteri.
+    public function __get($property) {
+        if (property_exists($this, $property))
+            return $this->$property;
+    }
+
+    public function __set($property, $value) {
+        if (property_exists($this, $property))
+            $this->$property = $value;
+
+        // Za omogućavanje ulančavanja.
+        return $this;
+    }
+}
+?>
